6498c6d48ddc848f7932a7427999172fc2d06984
josh <postor@gmail.com>
2018-03-15 15:42:11 +0800

    add puppeteer

--- tools/i18n-helper.js
+++ tools/i18n-helper.js
@@ -3,7 +3,17 @@ import getWrapper from 'next-i18n-helper/dist/wrapper'
 
 const i18nHelper = new I18nHelper({
   supportLangs: ['en', 'zh'],
-  defaultLang: 'en',
+  localesBaseUrl: '/locales',
+  i18nOption: {
+    cache: {
+      enabled: true,
+      expirationTime: 7 * 24 * 60 * 60 * 1000,
+      versions: {
+        en: '0.1',
+        zh: '0.1',
+      },
+    },
+  },
 })
 
 export default i18nHelper
